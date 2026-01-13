360b9172153c364a0747b4b5353768fa7643f084
Squaiera <lucasdanielsquaiera@gmail.com>
2026-01-13 22:39:35 +0100

    13/01 casa

--- EjemplosBasicos/src/basicos/Saludo.java
+++ EjemplosBasicos/src/basicos/Saludo.java
@@ -1,27 +1,23 @@
 package basicos;
 import java.util.Scanner;
 public class Saludo {
+	
+	public static String saludoMetodo(String n, String a) {
+		String saludo = "Hola " + n + " " + a;
+		
+		return saludo;
+	}
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		
 		String nombre;
 		String apellido;
-		
-		System.out.println("Por favor, escribe tu nombre");
-		 
-		nombre = entrada.nextLine();
-		
-		 System.out.println("Hola, " + nombre);
-		
-		System.out.println("Ahora, dime tu apellido.");
-		
-		apellido = entrada.nextLine();
-		System.out.println("Bienvenido a Java " + nombre + " "+ apellido );
-		
+		System.out.println("Por favor, escribe tu nombre:");
+		nombre = entrada.nextLine().toUpperCase();
+		System.out.println("Ahora, dime tu apellido:");
+		apellido = entrada.nextLine().toUpperCase();
+		System.out.println(saludoMetodo(nombre,apellido));
 		entrada.close();
-		
-
 	}
 
 }
